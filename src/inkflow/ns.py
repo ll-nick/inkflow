cf29448d0952c6451733e1e1c5286849ced40a36
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-06-16 11:46:03 +0200

    Add a default zone tag to the inkflow namespace

--- src/inkflow/ns.py
+++ src/inkflow/ns.py
@@ -5,5 +5,6 @@ INKSCAPE = "http://www.inkscape.org/namespaces/inkscape"
 SODIPODI = "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
 
 INKFLOW_PARENT = f"{{{INKFLOW}}}parent"
+INKFLOW_DEFAULT_ZONE = f"{{{INKFLOW}}}default-zone"
 INKFLOW_LAYOUT_SRC = f"{{{INKFLOW}}}layout-src"
 INKFLOW_LAYOUT_HASH = f"{{{INKFLOW}}}layout-hash"
